da1efad1fc63fb34d79aecaf823252d198acc309
misaki-lon <s1911360@s.tsukuba.ac.jp>
2022-10-04 11:42:23 +0900

    reader実装

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,47 @@
 <template>
-  <v-btn
-    elevation="2"
-  >カメラを起動</v-btn>
+  <div class="qrReader">
+    <qrcode-reader :paused="paused" @init="onInit" @decode="onDecode"></qrcode-reader>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'IndexPage',
-}
+  import { QrcodeReader } from 'vue-qrcode-reader'
+  export default {
+    name: "QrReader",
+    components: { QrcodeReader },
+    data () {
+      return {
+        paused: false
+      }
+    },
+    methods: {
+      async onInit (promise) {
+        // show loading indicator
+        try {
+          await promise
+          // successfully initialized
+        } catch (error) {
+          if (error.name === 'NotAllowedError') {
+            // user denied camera access permisson
+          } else if (error.name === 'NotFoundError') {
+            // no suitable camera device installed
+          } else if (error.name === 'NotSupportedError') {
+            // page is not served over HTTPS (or localhost)
+          } else if (error.name === 'NotReadableError') {
+            // maybe camera is already in use
+          } else if (error.name === 'OverconstrainedError') {
+            // passed constraints don't match any camera. Did you requested the front camera although there is none?
+          } else {
+            // browser is probably lacking features (WebRTC, Canvas)
+          }
+        } finally {
+          // hide loading indicator
+        }
+      },
+      onDecode(content){
+        this.paused = true
+        alert(content)
+      }
+    }
+  }
 </script>
